ff7bb321a8d6095eb4023d347ee8cb5f149f7cc4
Asiya Asha <asya.asha@gmail.com>
2017-08-13 18:37:50 -0400

    Update ag6394_hw7_q1.cpp

--- ag6394_hw7_q1.cpp
+++ ag6394_hw7_q1.cpp
@@ -4,27 +4,35 @@
 // This function is given a positive integer n, and
 // prints a textual image of a right triangle
 // (aligned to the left) made of n lines with asterisks.
-// For example,
-// printTriangle
-// should print:
-//   **
-//  ***
-//  ****
+// 
+// *
+// **
+// ***
+// ****
 //b. void printOpositeTriangles(int n)
 // This function is given a positive integer n, and
 // prints a textual image of a two opposite right
 // triangles (aligned to the left) with asterisks, each containing n lines.
 // For example,
 // ****
-// printOpositeTriangles
-// , should print:
 // ***
 // **
-// (4)
-//  * *
-//   **
-//  ***
-//  ****
+// *
+// **
+// ***
+// ****
+// c. printRuler()
+// -
+// --
+// -
+// ---
+// -
+// --
+// -
+// ---
+// -
+// --
+// -
 #include <iostream>
 using namespace std;
 
